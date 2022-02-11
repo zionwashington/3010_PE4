7f10450153fb58e3dd8e33d30357f1ea23605c1d
zionwashington <77951068+zionwashington@users.noreply.github.com>
2022-02-11 16:39:11 -0700

    Tic tac toe (#4)
    * File Upload TicTacToe
    
    * Display Board Added
    
    * GetPlayerChoice function added to file
    
    * changed to char
    
    Co-authored-by: jakenichols2719 <54028990+jakenichols2719@users.noreply.github.com>

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -5,28 +5,35 @@
 using namespace std;
 
 void createBoard(){    
-    int Board[3][3] = {};
+    char Board[3][3] = {};
+}
+
+char GetPlayerChoice(){
+    char choice; 
+    cin >> choice; 
+    return choice; 
+    
 }
 
 int** CreateBoard() {
     int** arr;
+    int number = 1;
     arr = new int*[3];
     for(int n=0; n<3; n++) {
         arr[n] = new int[3];
         for(int i=0; i<3; i++) {
-            arr[n][i] = 0;
+            arr[n][i] = '0'+ number++ ;
         }
     }
     return arr;
 }
 
 void PrintBoard(int** board) {
-    printf("%d %d %d\n%d %d %d\n%d %d %d\n",
+    printf("%c %c %c\n%c %c %c\n%c %c %c\n",
     board[0][0], board[0][1], board[0][2],
     board[1][0], board[1][1], board[1][2],
     board[2][0], board[2][1], board[2][2]);
 }
-
 void PlaceMarker(int** board, int x, int y, int marker) {
     board[y][x] = marker;
 }
@@ -34,5 +41,11 @@ void PlaceMarker(int** board, int x, int y, int marker) {
 int main(){
     int** board = CreateBoard();
     PrintBoard(board);
+
+    cout << "Choose a position to play." << endl;
+    GetPlayerChoice();
+
+
+
     return 0; 
 }
