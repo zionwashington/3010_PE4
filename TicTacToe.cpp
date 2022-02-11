d5218773e7c269799d7c8be868e64a82279f5f1b
zionwashington <zion.washington@colorado.edu>
2022-02-11 16:38:00 -0700

    changed to char

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 void createBoard(){    
-    int Board[3][3] = {};
+    char Board[3][3] = {};
 }
 
-string GetPlayerChoice(){
-    string choice; 
+char GetPlayerChoice(){
+    char choice; 
     cin >> choice; 
     return choice; 
     
@@ -21,14 +21,14 @@ int** CreateBoard() {
     for(int n=0; n<3; n++) {
         arr[n] = new int[3];
         for(int i=0; i<3; i++) {
-            arr[n][i] = number++ ;
+            arr[n][i] = '0'+ number++ ;
         }
     }
     return arr;
 }
 
 void PrintBoard(int** board) {
-    printf("%d %d %d\n%d %d %d\n%d %d %d\n",
+    printf("%c %c %c\n%c %c %c\n%c %c %c\n",
     board[0][0], board[0][1], board[0][2],
     board[1][0], board[1][1], board[1][2],
     board[2][0], board[2][1], board[2][2]);
