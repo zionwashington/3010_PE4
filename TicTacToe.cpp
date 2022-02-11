12d021d680754e42e59628626cc2e00519e3ebab
zionwashington <zion.washington@colorado.edu>
2022-02-11 16:08:13 -0700

    Display Board Added

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,12 +3,29 @@
 
 using namespace std;
 
-void createBoard(){
-        
-        int Board[3][3] = {};
+void DisplayBoard(int board[3][3]){
+    
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<3; j++){
+            cout << "|" << board[i][j] << "|";
+            if(j==2)
+            {
+                cout << endl;
+            }
+        }
+    }
+
 }
+void createBoard(){
+
+    int Board[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+    DisplayBoard(Board);
+}
+
 int main(){
 
     createBoard();
+    
+
     return 0; 
 }
